cbb94175758478196d40111a7ced4db213cbc58b
Pedro Henrique <pedromotoehm@gmail.com>
2022-03-14 13:40:59 -0300

    local storage add

--- src/components/main.js
+++ src/components/main.js
@@ -14,9 +14,23 @@ export default class Main extends Component {
     tarefas: [],
     index: -1,
   };
+
+  componentDidMount() {
+    const tarefas = JSON.parse(localStorage.getItem('tarefas'))
+
+    if (!tarefas) return
+    this.setState({tarefas})
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { tarefas } = this.state;
+    if (tarefas === prevState.tarefas) return
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+  }
+
   handleSubmit = (e) => {
     e.preventDefault()
-    const { tarefas , index} = this.state
+    const { tarefas, index } = this.state
     let { novaTarefa } = this.state
     novaTarefa = novaTarefa.trim();
     if (tarefas.indexOf(novaTarefa) !== -1) return;
